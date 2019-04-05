0acf9a3517bffa842da2815decb0053bacd19ab0
yogeshsingla <yogeshsingla131@gmail.com>
2019-04-05 14:22:50 +0530

    add rent module

---
+++ lab9/TAVS/webapp/mymodule/User.py
@@ -0,0 +1,6 @@
+def User:
+	"Stores every user detail"
+	def __init__():
+    	self.name = ''
+    	self.dl = '0'
+    	self.vehicle_booked = ''
---
+++ lab9/TAVS/webapp/mymodule/Vehicle.py
@@ -0,0 +1,6 @@
+def Vehicle:
+	"Stores every user detail"
+	def __init__():
+    	self.name = ''
+    	self.dl = '0'
+    	self.vehicle_booked = ''
